c449a445b55c9186965b1663801055e9c4225e36
dearasyouwish-ceo <dearasyouwish@gmail.com>
2026-08-23 22:25:43 +0530

    chore: remove placeholder corrected schema file

--- supabase_schema_fixed.sql
+++
@@ -1,4 +0,0 @@
--- VidyaDesk corrected schema generated from the uploaded supabase_schema.sql.
--- Fixes generic RLS policies for tables that do not have a direct institute_id.
--- Run supabase_fresh_reset.sql first, then this file.
-
